bbc0bc908ba1e4b2fe5ad23810860d21c660fccb
Noctisae <fanch.toquer@laposte.net>
2016-02-24 22:55:37 +0100

    Ajout de la vérification toutes les secondes

--- bash.sh
+++ bash.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-if [ ps up `cat /tmp/mydaemon.pid ` >/dev/null ]
-  then
-    echo "Python process already running" >> /var/log/application.log
-else
-    /usr/bin/python /home/debian/robot.py
-fi
+i="0"
+while [ "$i" -lt 60 ]
+do
+  if [ ps up `cat /tmp/mydaemon.pid ` >/dev/null ]
+    then
+      echo "Python process already running" >> /var/log/application.log
+  else
+      /usr/bin/python /home/debian/robot.py
+  fi
+  i=$[$i+1]
+  sleep 1
+done
