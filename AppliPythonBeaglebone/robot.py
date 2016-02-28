007f804cdb5fcd75fb03d9283e6c3c1d676a73a3
Noctisae <fanch.toquer@laposte.net>
2016-02-28 19:18:21 +0100

    Ajout de curses, redéfinition des classes

--- AppliPythonBeaglebone/robot.py
+++ AppliPythonBeaglebone/robot.py
@@ -2,6 +2,7 @@
 import Adafruit_BBIO.UART as UART
 import serial
 import socket
+import curses
 
 import os
 import sys
@@ -13,10 +14,9 @@ class Robot(object):
 	Classe regroupant l'application générale du robot
 		Contient:
 			Une connexion série UART
-			Une connexion socket serveur chargée de récupérer les ordres de l'application Android
-			Une connexion socket client chargée d'envoyer les informations à l'application Android
+			L'algorythmique de surveillance et la transmission des commandes aux moteurs
 	"""
-	def __init(self):
+	def __init__(self):
 		### Liaison série
 		#On choisir la liaison série 4
 		UART.setup("UART4")
@@ -25,6 +25,24 @@ class Robot(object):
 		self.ser = serial.Serial(port = "/dev/ttyO4", baudrate=9600)
 		self.ser.close()
 
+	#Fonction chargée d'effectuer l'envoi sur la liaison série des commandes moteurs
+	def ordre_moteurs(self,commande):
+		self.ser.open()
+		if self.ser.isOpen():
+			self.ser.write(commande)
+			#Si la commande est de connaitre la vitesse des moteurs 1 ou 2, on retourne cette valeur
+			if (commande == '0x21' || commande == '0x22'):
+					return self.ser.read()
+		self.ser.close()
+
+class Socket(object):
+	"""
+	Classe englobant le socket serveur permettant la transmission d'infos du robot au smartphone, et inversement
+		Contient:
+			Socket serveur réceptionnant les informations du smartphone
+			Socket client transmettant les informations vers le smartphone
+	"""
+	def __init__(self):
 		### Socket serveur
 		self.socket_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -46,19 +64,15 @@ class Robot(object):
 		#Idée : attribuer une IP fixe grâce au routeur au téléphone Android, voir réseau en 255.255.255.252
 		self.socket_client.connect(('192.168.1.3', 12800))
 
-
-	#Fonction chargée d'effectuer l'envoi sur la liaison série des comamndes moteurs
-	def ordre_moteurs(self,commande):
-		self.ser.open()
-		if self.ser.isOpen():
-			self.ser.write(commande)
-			#Si la commande est de connaitre la vitesse des moteurs 1 ou 2, on retourne cette valeur
-			if (commande == '0x21' || commande == '0x22'):
-					return self.ser.read()
-		self.ser.close()
+	#TODO
+	def transmission(self,destination,infos):
+		#Soit on envoie au robot, soit on envoie au smartphone
+		if destination == "robot":
+			return self.socket_serveur.recv(1024)
+		else:
+			self.socket_client.send(infos)
 
 ### Programme principal
-
 if __name__ == '__main__':
 	#On prépare un fichier temporaire tant que le script est lancé
 	pid = str(os.getpid())
@@ -70,7 +84,18 @@ if __name__ == '__main__':
 	file(pidfile, 'w').write(pid)
 	#On effectue le vrai travail ici
 	try:
-	    temp = Robot()
-	#Quand on a fini toute l'application (si celle-ci a une fin), on efface le fichier disant que l'application est lancée
+	    robot = Robot()
+		socket = Socket()
+		#on initialise la fenetre graphique mode terminal
+		stdscr = curses.initscr()
+		curses.noecho()
+		curses.cbreak()
+		stdscr.keypad(1)
+
+	#Quand on a fini toute l'application (si celle-ci a une fin), on efface le fichier disant que l'application est lancée (et on restaure le terminal initial)
 	finally:
+		curses.nocbreak()
+		stdscr.keypad(0)
+		curses.echo()
+		curses.endwin()
 	    os.unlink(pidfile)
