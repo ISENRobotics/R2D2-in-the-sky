b501381f3818346afb6cc319de11c47f712b7701
Noctisae <fanch.toquer@laposte.net>
2016-03-02 13:47:02 +0100

    Début de la détection des touches

--- AppliPythonBeaglebone/robot.py
+++ AppliPythonBeaglebone/robot.py
@@ -88,10 +88,40 @@ if __name__ == '__main__':
 		socket = Socket()
 		#on initialise la fenetre graphique mode terminal
 		stdscr = curses.initscr()
+		#On n'affiche pas les entrées saisies à l'écran
 		curses.noecho()
 		curses.cbreak()
+		#On active le mode keypad, permettant la transcription automatique des touches spéciales telles que les flèches en "propriétés curses"
 		stdscr.keypad(1)
-
+		#On fait en sorte que getch() ne soit pas bloquant
+		stdscr.nodelay(True)
+		continue = True
+		while continue:
+			#On récupère l'entrée utilisateur
+			c = stdscr.getch()
+			if c == curses.KEY_LEFT:
+				robot.ordre_moteurs(robot,"x21")
+				robot.ordre_moteurs(robot,"x21")
+				robot.ordre_moteurs(robot,"x21")
+				robot.ordre_moteurs(robot,"x21")
+			elif c == curses.KEY_RIGHT:
+				robot.ordre_moteurs(robot,"x21")
+				robot.ordre_moteurs(robot,"x21")
+				robot.ordre_moteurs(robot,"x21")
+				robot.ordre_moteurs(robot,"x21")
+			elif c == curses.KEY_UP:
+				robot.ordre_moteurs(robot,"x21")
+				robot.ordre_moteurs(robot,"x21")
+				robot.ordre_moteurs(robot,"x21")
+				robot.ordre_moteurs(robot,"x21")
+			elif c == curses.KEY_DOWN:
+				robot.ordre_moteurs(robot,"x21")
+				robot.ordre_moteurs(robot,"x21")
+				robot.ordre_moteurs(robot,"x21")
+				robot.ordre_moteurs(robot,"x21")
+			elif c == curses.KEY_ESC
+				continue = False
+			pass
 	#Quand on a fini toute l'application (si celle-ci a une fin), on efface le fichier disant que l'application est lancée (et on restaure le terminal initial)
 	finally:
 		curses.nocbreak()
